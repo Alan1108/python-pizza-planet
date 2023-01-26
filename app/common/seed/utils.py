8b4bb96d0a21f4581e583961ff7afbc4dec43a2d
Alan1108 <alan.ibm1108@gmail.com>
2023-01-26 08:58:57 -0500

    The structure for the seed script was added and also the dependency for Faker library

---
+++ app/common/seed/utils.py
@@ -0,0 +1,4 @@
+def get_random_sequence(length: int = 10) -> str:
+    digits = list(map(str, range(10)))
+    sequence = [digits[random.randint(0, 9)] for _ in range(length)]
+    return ''.join(sequence)
